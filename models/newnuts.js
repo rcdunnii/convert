3fd065ca6e276e72d9888f607625f2d0022d791e
Robert Dunn <rcdunnii@gmail.com>
2015-01-13 12:15:15 -0600

    more work

--- models/newnuts.js
+++ models/newnuts.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 // registering our nuts schema with mongoose
 
-newnutsSchema = new Schema({
+newnutsSchema1 = new Schema({
 	 "_id": Schema.ObjectId,
 	"walnutID": String,
     "visibility": Boolean,
@@ -12,24 +12,60 @@ newnutsSchema = new Schema({
     "Children": String,
     "Address" :
       [
-     	{"_id":Schema.ObjectId, "Line" : String},
-     	{"_id":Schema.ObjectId, "Line" : String},
-     	{"_id":Schema.ObjectId, "Line" : String},
-     	{"_id":Schema.ObjectId, "Line" : String}
+     	{"_id":String, "Line" : String},
+     	{"_id":String, "Line" : String},
+     	{"_id":String, "Line" : String},
+     	{"_id":String, "Line" : String}
    	  ],
 	Email  : 
-	 [  {"_id":Schema.ObjectId, "Email"	: String},
-	    {"_id":Schema.ObjectId, "Email"	: String},
-	 	{"_id":Schema.ObjectId, "Email"	: String},
+	 [  {"_id":String, "Email"	: String},
+	    {"_id":String, "Email"	: String},
+	 	{"_id":String, "Email"	: String},
 	 ],
 	Phone  :
 	  [ 
- 		{"_id":Schema.ObjectId, "Phone": String},
- 	    {"_id":Schema.ObjectId, "Phone": String}
+ 		{"_id":String, "Phone": String},
+ 	    {"_id":String, "Phone": String}
  	  ],
     "Notes": String,
     "Created": Date,
     "Updated": Date		
 });
 
- Newnuts = mongoose.model('Newnuts', newnutsSchema);
+newnutsSchema2 = new Schema({
+     "_id": Schema.ObjectId,
+    "walnutID": String,
+    "visibility": Boolean,
+    "SirName": String,
+    "Names": String,
+    "FormalNames": String,
+    "Children": String,
+    "Address" : {
+        {"_id": Schema.ObjectId},
+      [
+        
+        {"Line" : String},
+        {"Line" : String},
+        {"Line" : String},
+        {"Line" : String}
+      ]
+                },
+    "Email" : 
+     [ 
+        {"_id": Schema.ObjectId},
+        {"Email" : String},
+        {"Email" : String},
+        {"Email" : String},
+     ],
+    "Phone"  :
+      [
+        {"_id": Schema.ObjectId},
+        {"Phone": String},
+        {"Phone": String}
+      ],
+    "Notes": String,
+    "Created": Date,
+    "Updated": Date     
+});
+Newnuts1 = mongoose.model('newnuts1', newnutsSchema1);
+Newnuts2 = mongoose.model('newnuts2', newnutsSchema2);
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,17 @@
 
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectID = require('mongodb').ObjectID;
+
 var db =  require('../db');
  db.on('error', console.error.bind(console, 'connection error:'));
  db.once('open', function () {
  /* var db = mongoose.connection;*/
- var newcollection = db.collection("newnuts");
+ var newcollection1 = db.collection("newnuts1");
+ var newcollection2 = db.collection("newnuts2");
+
  console.log("opened connection to mongolab chestnuts");
   
 
@@ -26,15 +32,17 @@ var db =  require('../db');
 
 				function sendToNewNuts(docs) { /* conveert found docs to new schema Newnuts, put them into array and send to newnuts collection */
 					
-					var thisDocArr = [];
+					var thisDocArr1  = [];
+					var thisDocArr2  = [];
 
 					for (var i = 0; i < docs.length; i++) {
 
-			 			var thisDoc = ("thisDoc_" + i);					 			
+			 			// var thisDoc = ("thisDoc_" + i);					 			
 
-			 			thisDoc = new Newnuts;
+			 			thisDoc1 = new Newnuts1;
+			 			thisDoc2 = new Newnuts2;
 						
-			 			thisDoc =  {
+			 			thisDoc1 =  {
 		             	// Newnuts.update({},  {			 
 							"walnutID": docs[i].walnutID,							
 		    				"visibility": docs[i].visibility,
@@ -44,35 +52,83 @@ var db =  require('../db');
 		    				"Children": docs[i].Children,
 		    				"Address" :
 		      				[
-		     					{"_id" : 1, "Line"	: docs[i].Addr1},
-		     					{"_id" : 2, "Line"  : docs[i].Addr2},
-		     					{"_id" : 3, "Line"	: docs[i].Addr3},
-		     					{"_id" : 4, "Line"	: docs[i].Addr4}
+		     					{"_id" : "1", "Line"	: docs[i].Addr1},
+		     					{"_id" : "2", "Line" 	: docs[i].Addr2},
+		     					{"_id" : "3", "Line"	: docs[i].Addr3},
+		     					{"_id" : "4", "Line"	: docs[i].Addr4}
 		   	  				],
 					 		"Email"  :
-			 	 			[  {"_id" : 1, "Email"	: docs[i].Email1},
-			 	 			   {"_id" : 2, "Email"	: docs[i].Email2},
-			 	 			   {"_id" : 3, "Email"	: docs[i].Email3},
+			 	 			[  {"_id" : "1", "Email"	: docs[i].Email1},
+			 	 			   {"_id" : "2", "Email"	: docs[i].Email2},
+			 	 			   {"_id" : "3", "Email"	: docs[i].Email3},
 			 	 			],
 					 		"Phone"  :
 					 		 [ 
-		 		 			   {"_id" : 1,	"Phone"  : docs[i].Phone1},
-		 	   				   {"_id" : 2,	"Phone"  : docs[i].Phone2}
+		 		 			   {"_id" :"1",	"Phone"  : docs[i].Phone1},
+		 	   				   {"_id" :"2",	"Phone"  : docs[i].Phone2}
+		 	 				],
+		    				"Notes": docs[i].Notes,
+		    				"Created": docs[i].Created,
+		    				"Updated": docs[i].Updated
+				 		}; /* thisDoc */
+
+
+				 		thisDoc2 =  {
+		             	// Newnuts.update({},  {			 
+							"walnutID": docs[i].walnutID,							
+		    				"visibility": docs[i].visibility,
+		    				"SirName":  docs[i].SirName,
+		    				"Names": docs[i].Names,
+		    				"FormalNames": docs[i].FormalNames,
+		    				"Children": docs[i].Children,
+		    				"Address" : {
+		    					"_id": ObjectId(),
+		      				[
+		      					
+		     					{"Line"	: docs[i].Addr1},
+		     					{"Line" : docs[i].Addr2},
+		     					{"Line"	: docs[i].Addr3},
+		     					{"Line"	: docs[i].Addr4}
+		   	  				]
+		   	  							},
+					 		"Email"  :
+			 	 			[ 
+			 	 			   
+			 	 			   {"Email"	: docs[i].Email1},
+			 	 			   {"Email"	: docs[i].Email2},
+			 	 			   {"Email"	: docs[i].Email3},
+			 	 			],
+					 		"Phone"  :
+					 		 [ 
+					 		   
+		 		 			   {"Phone"  : docs[i].Phone1},
+		 	   				   {"Phone"  : docs[i].Phone2}
 		 	 				],
 		    				"Notes": docs[i].Notes,
 		    				"Created": docs[i].Created,
 		    				"Updated": docs[i].Updated
 				 		}; /* thisDoc */
 			 		
-			 			thisDocArr.push(thisDoc);  // put new doc into array
+			 			thisDocArr1.push(thisDoc1);  // put new doc into array
+			 			thisDocArr2.push(thisDoc2);
 
 		 			}; /* for */
 		 			    /* mongoose update fails - had to use node driver which adds whole array of docs! */
-						newcollection.insert(thisDocArr, function(err, records) {
+						newcollection1.insert(thisDocArr1, function(err, records) {
 							 if (err) {
 							 	return handleError(err);
 							  } else {
-							   	res.send("Inserted " + thisDocArr.length + " documents into newnuts collection of chestnuts database")
+							   	// res.send("Inserted " + thisDocArr1.length + " documents into newnuts collection of chestnuts database")
+							   	// res.end();
+							   	// db.close();
+							  }
+						});
+
+						newcollection2.insert(thisDocArr2, function(err, records) {
+							 if (err) {
+							 	return handleError(err);
+							  } else {
+							   	res.send("Inserted " + records.length + " documents into newnuts collection of chestnuts database")
 							   	res.end();
 							   	// db.close();
 							  }
